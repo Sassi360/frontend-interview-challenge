79d1d382c79b156441f50d62c2e5d940a1528f2f
Mohamed Sassi <mohamed@msassi.dev>
2023-12-06 10:16:42 -0500

    Add pinia-plugin-persistedstate dependency

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { createApp } from 'vue'
 import App from "./App.vue"
 import "./assets/css/style.css"
 import { router } from './router'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 // Use the router
 app.use(router)
